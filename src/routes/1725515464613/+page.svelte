93d17bebbcc2555f8d6a7e8142fd63be4378ddc1
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2024-09-05 15:22:16 +0900

    feat: kakaotalk external browser demo

---
+++ src/routes/1725515464613/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let data;
+
+	const href = 'https://github.com/hyunbinseo/';
+	const text = 'GitHub@hyunbinseo';
+</script>
+
+{#if !data.isKakaoTalk}
+	<p>카카오톡 인앱 브라우저가 아닙니다.</p>
+	<a {href}>{text}</a>
+{:else}
+	<p>카카오톡 인앱 브라우저가 맞습니다.</p>
+	<a href="kakaotalk://web/openExternal?url={encodeURI(href)}">{text}</a>
+{/if}
---
+++ src/routes/1725515464613/+page.ts
@@ -0,0 +1,9 @@
+import { browser } from '$app/environment';
+import type { PageLoad } from './$types';
+
+export const ssr = false;
+
+export const load = (() => ({
+	meta: { title: '카카오톡 인앱 브라우저에서 기본 브라우저 열기', noindex: true },
+	isKakaoTalk: !browser ? undefined : window.navigator.userAgent.includes('KAKAOTALK')
+})) satisfies PageLoad;
