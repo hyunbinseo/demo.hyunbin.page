561edd78a7f9d9022da1bfab95416f0a6d5c5836
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2024-09-20 14:57:01 +0900

    fix: cannot read properties of undefined

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,16 +5,19 @@
 </script>
 
 <svelte:head>
-	{#if $page.data.meta.noindex !== true}
-		<title>{$page.data.meta.title} / {siteName}</title>
-		<meta property="og:title" content="[Demo] {$page.data.meta.title}" />
-		<meta name="description" content={$page.data.meta.description} />
-		<meta property="og:description" content={$page.data.meta.description} />
-		<meta property="og:site_name" content={siteName} />
-		<meta property="og:type" content="website" />
-	{:else}
-		<title>{$page.data.meta.title || siteName}</title>
-		<meta name="robots" content="noindex" />
+	{#if $page.data.meta}
+		{@const meta = $page.data.meta}
+		{#if !meta.noindex}
+			<title>{meta.title} / {siteName}</title>
+			<meta property="og:title" content="[Demo] {meta.title}" />
+			<meta name="description" content={meta.description} />
+			<meta property="og:description" content={meta.description} />
+			<meta property="og:site_name" content={siteName} />
+			<meta property="og:type" content="website" />
+		{:else}
+			<title>{meta.title || siteName}</title>
+			<meta name="robots" content="noindex" />
+		{/if}
 	{/if}
 </svelte:head>
 
